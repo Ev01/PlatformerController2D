6bdea4da7f64c523a7287ca1a4d0e9896b25c10c
Ev01 <evpevdev@optusnet.com.au>
2023-02-25 10:55:01 +1100

    Add documentation comments

--- platformer_controller/platformer_controller.gd
+++ platformer_controller/platformer_controller.gd
@@ -3,12 +3,16 @@ extends CharacterBody2D
 class_name PlatformerController2D
 
 # Set these to the name of your action (in the Input Map)
+## Name of input action to move left.
 @export var input_left : String = "move_left"
+## Name of input action to move right.
 @export var input_right : String = "move_right"
+## Name of input action to jump.
 @export var input_jump : String = "jump"
 
-# The max jump height in pixels (holding jump)
+
 var _max_jump_height: float = 150
+## The max jump height in pixels (holding jump).
 @export var max_jump_height: float: 
 	get:
 		return _max_jump_height
@@ -21,8 +25,9 @@ var _max_jump_height: float = 150
 		release_gravity_multiplier = calculate_release_gravity_multiplier(
 				jump_velocity, min_jump_height, default_gravity)
 			
-# The minimum jump height (tapping jump)
+
 var _min_jump_height: float = 40
+## The minimum jump height (tapping jump).
 @export var min_jump_height: float: 
 	get:
 		return _min_jump_height
@@ -32,8 +37,9 @@ var _min_jump_height: float = 40
 				jump_velocity, min_jump_height, default_gravity)
 
 
-# The height of your jump in the air
+
 var _double_jump_height: float = 100
+## The height of your jump in the air.
 @export var double_jump_height: float:
 	get:
 		return _double_jump_height
@@ -41,8 +47,9 @@ var _double_jump_height: float = 100
 		_double_jump_height = value
 		double_jump_velocity = calculate_jump_velocity2(double_jump_height, default_gravity)
 		
-# How long it takes to get to the peak of the jump in seconds
+
 var _jump_duration: float = 0.3
+## How long it takes to get to the peak of the jump in seconds.
 @export var jump_duration: float:
 	get:
 		return _jump_duration
@@ -55,17 +62,17 @@ var _jump_duration: float = 0.3
 		release_gravity_multiplier = calculate_release_gravity_multiplier(
 				jump_velocity, min_jump_height, default_gravity)
 		
-# Multiplies the gravity by this while falling
+## Multiplies the gravity by this while falling.
 @export var falling_gravity_multiplier = 1.5
-# Set to 2 for double jump
+## Amount of jumps allowed before needing to touch the ground again. Set to 2 for double jump.
 @export var max_jump_amount = 1
 @export var max_acceleration = 4000
 @export var friction = 8
 @export var can_hold_jump : bool = false
-# You can still jump this many seconds after falling unchecked a ledge
+## You can still jump this many seconds after falling off a ledge.
 @export var coyote_time : float = 0.1
-# Only neccessary when can_hold_jump is unchecked
-# Pressing jump this many seconds before hitting the ground will still make you jump
+## Pressing jump this many seconds before hitting the ground will still make you jump.
+## Only neccessary when can_hold_jump is unchecked.
 @export var jump_buffer : float = 0.1
 
 
@@ -167,47 +174,10 @@ func _physics_process(delta):
 	move_and_slide()
 
 
-
-func calculate_gravity(p_max_jump_height, p_jump_duration):
-	# Calculates the desired gravity by looking at our jump height and jump duration
-	# Formula is from this video https://www.youtube.com/watch?v=hG9SzQxaCm8
-	return (-2 *p_max_jump_height) / pow(p_jump_duration, 2)
-
-
-func calculate_jump_velocity(p_max_jump_height, p_jump_duration):
-	# Calculates the desired jump velocity by lookihg at our jump height and jump duration
-	return (2 * p_max_jump_height) / (p_jump_duration)
-
-
-func calculate_jump_velocity2(p_max_jump_height, p_gravity):
-	# Calculates jump velocity from jump height and gravity
-	# formula from 
-	# https://sciencing.com/acceleration-velocity-distance-7779124.html#:~:text=in%20every%20step.-,Starting%20from%3A,-v%5E2%3Du
-	return sqrt(-2 * p_gravity * p_max_jump_height)
-
-
-func calculate_release_gravity_multiplier(p_jump_velocity, p_min_jump_height, p_gravity):
-	# Calculates the gravity when the key is released based checked the minimum jump height and jump velocity
-	# Formula is from this website https://sciencing.com/acceleration-velocity-distance-7779124.html
-	var release_gravity = 0 - pow(p_jump_velocity, 2) / (2 * p_min_jump_height)
-	return release_gravity / p_gravity
-
-
-func calculate_friction(time_to_max):
-	# Formula from https://www.reddit.com/r/gamedev/comments/bdbery/comment/ekxw9g4/?utm_source=share&utm_medium=web2x&context=3
-	# this friction will hit 90% of max speed after the accel time
-	return 1 - (2.30259 / time_to_max)
-
-
-func calculate_speed(p_max_speed, p_friction):
-	# Formula from https://www.reddit.com/r/gamedev/comments/bdbery/comment/ekxw9g4/?utm_source=share&utm_medium=web2x&context=3	
-	return (p_max_speed / p_friction) - p_max_speed
-
-
 func jump():
 	if jumps_left == max_jump_amount and coyote_timer.is_stopped():
-		# Your first jump must be used when checked the ground
-		# If you fall unchecked the ground and then jump you will be using you second jump
+		# Your first jump must be used when on the ground
+		# If you fall off the ground and then jump you will be using your second jump
 		jumps_left -= 1
 		
 	if jumps_left > 0:
@@ -219,3 +189,43 @@ func jump():
 	
 	
 	coyote_timer.stop()
+
+
+
+## Calculates the desired gravity from jump height and jump duration.  [br]
+## Formula is from [url=https://www.youtube.com/watch?v=hG9SzQxaCm8]this video[/url] 
+func calculate_gravity(p_max_jump_height, p_jump_duration):
+	return (-2 *p_max_jump_height) / pow(p_jump_duration, 2)
+
+
+## Calculates the desired jump velocity from jump height and jump duration.
+func calculate_jump_velocity(p_max_jump_height, p_jump_duration):
+	return (2 * p_max_jump_height) / (p_jump_duration)
+
+
+## Calculates jump velocity from jump height and gravity.  [br]
+## Formula from 
+## [url]https://sciencing.com/acceleration-velocity-distance-7779124.html#:~:text=in%20every%20step.-,Starting%20from%3A,-v%5E2%3Du[/url]
+func calculate_jump_velocity2(p_max_jump_height, p_gravity):
+	return sqrt(-2 * p_gravity * p_max_jump_height)
+
+
+## Calculates the gravity when the key is released based off the minimum jump height and jump velocity.  [br]
+## Formula is from [url]https://sciencing.com/acceleration-velocity-distance-7779124.html[/url]
+func calculate_release_gravity_multiplier(p_jump_velocity, p_min_jump_height, p_gravity):
+	var release_gravity = 0 - pow(p_jump_velocity, 2) / (2 * p_min_jump_height)
+	return release_gravity / p_gravity
+
+
+## Returns a value for friction that will hit the max speed after 90% of time_to_max seconds.  [br]
+## Formula from [url]https://www.reddit.com/r/gamedev/comments/bdbery/comment/ekxw9g4/?utm_source=share&utm_medium=web2x&context=3[/url]
+func calculate_friction(time_to_max):
+	return 1 - (2.30259 / time_to_max)
+
+
+## Formula from [url]https://www.reddit.com/r/gamedev/comments/bdbery/comment/ekxw9g4/?utm_source=share&utm_medium=web2x&context=3[/url]
+func calculate_speed(p_max_speed, p_friction):
+	return (p_max_speed / p_friction) - p_max_speed
+
+
+
