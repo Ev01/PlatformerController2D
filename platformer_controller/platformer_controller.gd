c8e586bd7d71f3611923b3b55344f24faf60146c
Ev01 <evpevdev@optusnet.com.au>
2023-04-19 17:15:41 +1000

    Remove Input checks from can_jump() function, and move them into _input

--- platformer_controller/platformer_controller.gd
+++ platformer_controller/platformer_controller.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 class_name PlatformerController2D
 
 signal jumped(is_ground_jump: bool)
+signal hit_ground()
 
 
 # Set these to the name of your action (in the Input Map)
@@ -18,6 +19,8 @@ const DEFAULT_MAX_JUMP_HEIGHT = 150
 const DEFAULT_MIN_JUMP_HEIGHT = 60
 const DEFAULT_DOUBLE_JUMP_HEIGHT = 100
 const DEFAULT_JUMP_DURATION = 0.3
+
+enum JumpType {NONE, GROUND, AIR}
 
 
 var _max_jump_height: float = DEFAULT_MAX_JUMP_HEIGHT
@@ -91,6 +94,7 @@ var acceleration_time = 10
 
 
 # These will be calcualted automatically
+# Gravity will be positive if it's going down, and negative if it's going up
 var default_gravity : float
 var jump_velocity : float
 var double_jump_velocity : float
@@ -100,7 +104,10 @@ var release_gravity_multiplier : float
 
 var jumps_left : int
 var holding_jump := false
+var current_jump_type: JumpType = JumpType.NONE
 
+# Used to detect if player just hit the ground
+var _was_on_ground: bool
 
 var acc = Vector2()
 
@@ -111,11 +118,9 @@ var acc = Vector2()
 func _init():
 	default_gravity = calculate_gravity(max_jump_height, jump_duration)
 	jump_velocity = calculate_jump_velocity(max_jump_height, jump_duration)
-	
-	
 	double_jump_velocity = calculate_jump_velocity2(double_jump_height, default_gravity)
 	release_gravity_multiplier = calculate_release_gravity_multiplier(
-		jump_velocity, min_jump_height, default_gravity)
+			jump_velocity, min_jump_height, default_gravity)
 
 
 func _ready():
@@ -136,21 +141,34 @@ func _input(_event):
 	if Input.is_action_pressed(input_right):
 		acc.x = max_acceleration
 	
+	if Input.is_action_pressed(input_jump):
+		if can_ground_jump() and can_hold_jump:
+			jump()
+	
 	if Input.is_action_just_pressed(input_jump):
 		holding_jump = true
 		jump_buffer_timer.start()
+		if can_ground_jump() or can_double_jump():
+			jump()
+		
 	if Input.is_action_just_released(input_jump):
 		holding_jump = false
 
 
 func _physics_process(delta):
-	if is_feet_on_ground():
+	if is_feet_on_ground() and current_jump_type == JumpType.NONE:
 		coyote_timer.start()
 	if not coyote_timer.is_stopped():
 		jumps_left = max_jump_amount
 	
-	if can_jump():
-		jump()
+	# Check if we just hit the ground this frame
+	if not _was_on_ground and is_feet_on_ground():
+		current_jump_type = JumpType.NONE
+		if not jump_buffer_timer.is_stopped(): 
+			jump()
+		
+		hit_ground.emit()
+	
 	
 	var gravity = apply_gravity_multipliers_to(default_gravity)
 	acc.y = -gravity
@@ -158,29 +176,22 @@ func _physics_process(delta):
 	# Apply friction
 	velocity.x *= 1 / (1 + (delta * friction))
 	velocity += acc * delta
-
+	
+	
+	_was_on_ground = is_feet_on_ground()
 	move_and_slide()
 
 
-func can_jump() -> bool:
-	if jumps_left > 0:
-		# Check for ground jumps when we cannot hold jump
-		if not can_hold_jump:
-			if not jump_buffer_timer.is_stopped() and is_feet_on_ground():
-				return true
-		
-		# Check for ground jumps when we can hold jump
-		if can_hold_jump:
-			if Input.is_action_pressed(input_jump):
-				# Dont use double jump when holding down
-				if is_feet_on_ground():
-					return true
-		
-		# Check for jumps in the air
-		if Input.is_action_just_pressed(input_jump):
-			if not is_feet_on_ground():
-				print("air jump")
-				return true
+func can_ground_jump() -> bool:
+	if jumps_left > 0 and is_feet_on_ground():
+		return true
+	
+	return false
+
+
+func can_double_jump():
+	if jumps_left > 0 and not is_feet_on_ground():
+		return true
 	
 	return false
 
@@ -193,7 +204,6 @@ func is_feet_on_ground():
 		return true
 	
 	return false
-	
 
 
 func jump():
@@ -202,28 +212,31 @@ func jump():
 		# If your first jump is used in the air, an additional jump will be taken away.
 		jumps_left -= 1
 		
-	if jumps_left < max_jump_amount: # If we are double jumping
+	if not is_feet_on_ground() and coyote_timer.is_stopped(): # If we are double jumping
 		velocity.y = -double_jump_velocity
-		emit_signal("jumped", false)
+		current_jump_type = JumpType.AIR
+		jumped.emit(false)
 	else:
 		velocity.y = -jump_velocity
-		emit_signal("jumped", true)
+		current_jump_type = JumpType.GROUND
+		jumped.emit(true)
 	
 	jumps_left -= 1
 	
 	coyote_timer.stop()
+	
 
 
 func apply_gravity_multipliers_to(gravity) -> float:
-	
 	if velocity.y * -sign(default_gravity) > 0: # If we are falling
 		gravity *= falling_gravity_multiplier
 	
 	# if we released jump and are still rising
 	elif velocity.y * -sign(default_gravity) < 0:
 		if not holding_jump: 
-			if not jumps_left < max_jump_amount - 1: # Always jump to max height when we are using a double jump
+			if not current_jump_type == JumpType.AIR: # Always jump to max height when we are using a double jump
 				gravity *= release_gravity_multiplier # multiply the gravity so we have a lower jump
+	
 	
 	return gravity
 
@@ -231,7 +244,7 @@ func apply_gravity_multipliers_to(gravity) -> float:
 ## Calculates the desired gravity from jump height and jump duration.  [br]
 ## Formula is from [url=https://www.youtube.com/watch?v=hG9SzQxaCm8]this video[/url] 
 func calculate_gravity(p_max_jump_height, p_jump_duration):
-	return (-2 *p_max_jump_height) / pow(p_jump_duration, 2)
+	return (-2 * p_max_jump_height) / pow(p_jump_duration, 2)
 
 
 ## Calculates the desired jump velocity from jump height and jump duration.
